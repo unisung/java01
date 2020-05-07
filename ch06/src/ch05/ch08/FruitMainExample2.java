d283a3d0c752928d39e94c4c4857d8c85675ac47
unisung <vctor@naver.com>
2020-05-07 17:40:31 +0900

    0507-4

--- ch06/src/ch05/ch08/FruitMainExample2.java
+++ ch06/src/ch05/ch08/FruitMainExample2.java
@@ -1,4 +1,7 @@
 package ch05.ch08;
+
+import java.util.Scanner;
+
 //Scanner로 값을 입력받아서 Fruit객체를 생성하여 배열에 저장하고,
 //판매수량에 따른 오름차순 정렬
 //판매량이 가장 적은 과일과
@@ -6,10 +9,17 @@ package ch05.ch08;
 //총수판매액과 평균판매액을 출력
 public class FruitMainExample2 {
 	public static void main(String[] args) {
-		Fruit apple=new Fruit("apple", 2000, Brix.LOW);
+		Scanner scanner = new Scanner(System.in);
+		String kind=scanner.next();
+	    int price =scanner.nextInt();
+	    String brix =scanner.next();
+		
+		Fruit apple=new Fruit(kind, price, Brix.valueOf(brix));
 		Fruit banana=new Fruit("banana",5000,Brix.valueOf("MEDIUM"));
 		Fruit mango=new Fruit("mango",10000,Brix.valueOf("HIGH"));
 		Fruit grape=new Fruit("grape",7000,Brix.valueOf("HIGH"));
+		
+		
 		
 		System.out.println(grape);
 		//판매수량
